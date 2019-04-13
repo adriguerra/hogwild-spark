f85a14bb2497198d61c6456c99583a204c511f6e
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-04-13 19:37:56 +0200

    Correct is_support bug

--- src/main/scala/SGD.scala
+++ src/main/scala/SGD.scala
@@ -13,18 +13,22 @@ object SGD {
     wsub.map(i => i - alpha * grads/batch_size)
   }
 
-  def compute_loss(train_XY: Vector[(Int, (Map[Int, Float], Int))], W: Vector[Double]) = {
+  def compute_loss(train_XY: Vector[(Int, (Map[Int, Float], Int))], W: Vector[Double], regParam: Double) = {
     val wsub = W
-    hinge_loss(train_XY, wsub).sum
+    val loss = hinge_loss(train_XY, wsub).sum
+    println("Compute loss: " + loss)
+    val reg = wsub.map(w => w * w).sum * regParam / 2
+    println("reg: " + reg)
+    reg + loss
   }
 
   def is_support(yn: Double, xn: Map[Int, Float], w: Vector[Double]) = {
-    xn.map(x => w(x._1) * x._2).sum < 1
+    yn * xn.map(x => w(x._1) * x._2).sum < 1
   }
 
   def compute_gradient(xn: Map[Int, Float], yn: Double, wsub: Vector[Double], regParam: Double, N: Double) = {
-    val grad = xn.mapValues(xi => N * {if(is_support(yn, xn, wsub)) xi * -yn else 0})
-    grad.map(x => x._2 + regParam*wsub(x._1)).toVector
+    val grad = xn.mapValues(xi => 1/N * { if(is_support(yn, xn, wsub)) xi * -yn else 0})
+    grad.map(x => x._2 + regParam * wsub(x._1)).toVector
   }
 
   def hinge_loss(XY: Vector[(Int, (Map[Int, Float], Int))], w: Vector[Double]) = {

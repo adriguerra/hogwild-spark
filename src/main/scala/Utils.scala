37e75806c14bd535929997ce7ed7665e5ecf1c92
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-04-13 11:02:45 +0200

    Add load_sample_reuters_data for RDDs

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -22,6 +22,29 @@ object Utils {
     }).collect().toList.unzip
   }
 
+  def load_sample_reuters_data(sc: SparkContext, train_path: String, topics_path: String, test_paths: List[String], selected_cat: String, train: Boolean) = {
+    val (labels, mappings) = {
+      if (train) {
+        val source = sc.textFile(train_path).take(20)
+        generate_labelled_data(sc.parallelize(source))
+      }
+      else {
+        var labels_tmp = List[Int]()
+        var data_i = List[Map[Int, Float]]()
+        for (path <- test_paths) {
+          val source = sc.textFile(path).take(20)
+          val (label, mapping) = generate_labelled_data(sc.parallelize(source))
+          data_i ++= mapping
+          labels_tmp ++= label
+        }
+        (labels_tmp, data_i)
+      }}
+
+    val categories = get_category_dict(topics_path)
+    val cat_labels = labels.map(label => if (categories.get(label).contains(selected_cat)) 1 else -1)
+    sc.parallelize(labels.zip(mappings.zip(cat_labels)))
+  }
+
   def load_reuters_data(sc: SparkContext, train_path: String, topics_path: String, test_paths: List[String], selected_cat: String, train: Boolean) = {
     val (labels, mappings) = {
       if (train) {
